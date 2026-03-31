fd80edc63455a5dc77d9ed406efab550da45c7a3
SimbaMupfu <scmupfururirwa@gmail.com>
2026-03-31 03:54:34 +0200

    Create the counter component and style the button and text.

---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,19 @@
+import './Counter.css'
+
+export default function Counter(){   
+    
+    function incrementCounter(){
+    console.log('increment clicked')
+}
+
+    return (
+        <div className="Counter">
+            <span className="count">0</span>
+            <div>
+                <button 
+                className="counterButton" 
+                onClick={incrementCounter}>+1</button>
+            </div>
+        </div>
+    )
+}
