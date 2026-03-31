9d6fa8634b77b53080807f28fae1da3ca791b22a
SimbaMupfu <scmupfururirwa@gmail.com>
2026-03-31 17:20:09 +0200

    Add state for the counter component when incrementing the count value.

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react'
 import './Counter.css'
 
-export default function Counter(){   
+export default function Counter(){  
+    
+    const [count, setCount] = useState(0)
     
     function incrementCounter(){
-    console.log('increment clicked')
+        setCount(count + 1)
 }
 
     return (
         <div className="Counter">
-            <span className="count">0</span>
+            <span className="count">{count}</span>
             <div>
                 <button 
                 className="counterButton" 
